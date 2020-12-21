3e3d4475360488a4fc11f64f2edc6ad028665190
Luke Berndt <lberndt@iqt.org>
2020-12-21 16:39:08 -0500

    adding azimuth calculations

--- ADS-B-funhouse/utils.py
+++ ADS-B-funhouse/utils.py
@@ -17,6 +17,11 @@ def deg2rad(deg: float) -> float:
     """
     return deg * (math.pi/180)
 
+def azimuth(distance: float, altitude):
+    baseElevation = 0
+    ratio = ( altitude - baseElevation) / distance
+    a = math.atan(ratio) * (180 /math.pi)
+    return a
 
 def bearing(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
     """Calculate bearing from lat1/lon2 to lat2/lon2
