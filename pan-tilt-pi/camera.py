b5436c22b3d3f88ec8a2d00b88ebea8edcab85d4
Luke Berndt <lberndt@iqt.org>
2020-12-22 11:20:00 -0500

    Update camera.py

--- pan-tilt-pi/camera.py
+++ pan-tilt-pi/camera.py
@@ -31,6 +31,7 @@ def on_message(client, userdata, message):
     command = str(message.payload.decode("utf-8"))
     update = json.loads(command)
     logging.info(update)
+    logging.info("Bearing: $d Azimuth: $f" % update["bearing"] % update["azimuth"])
     if (update["bearing"] < 90):
         logging.info("Setting Pan to: $d" % update["bearing"])
         pantilthat.pan(update["bearing"])
