87f2c0c2533f97da10692b48eb6eeca077a83665
Luke Berndt <lberndt@iqt.org>
2021-03-03 10:46:04 -0500

    Update install_requirements.sh
    Former-commit-id: 90c7d50ec842c1683b2b68cd8ef94846f43662c5

--- object-tracker/opencv/install_requirements.sh
+++ object-tracker/opencv/install_requirements.sh
@@ -22,12 +22,13 @@ if grep -s -q "Mendel" /etc/os-release; then
     exit 1
   fi
   sudo apt install python3-opencv
+  sudo pip3 install paho-mqtt
 elif grep -s -q "Raspberry Pi" /sys/firmware/devicetree/base/model; then
   RASPBIAN=$(grep VERSION_ID /etc/os-release | sed 's/VERSION_ID="\([0-9]\+\)"/\1/')
   echo "Raspbian Version: $RASPBIAN"
   if [[ "$RASPBIAN" -ge "10" ]]; then
     # Lock to version due to bug: https://github.com/piwheels/packages/issues/59
-    sudo pip3 install opencv-contrib-python==4.1.0.25
+    sudo pip3 install opencv-contrib-python==4.1.0.25 paho-mqtt
     sudo apt-get -y install libjasper1 libhdf5-1* libqtgui4 libatlas-base-dev libqt4-test
   else
     echo "For Raspbian versions older than Buster (10) you have to build OpenCV yourself"
