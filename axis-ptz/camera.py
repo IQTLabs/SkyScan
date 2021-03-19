99950597b1144e8ea6cbb0246c5ab43e8b3f4b1d
Luke Berndt <lberndt@iqt.org>
2021-03-19 15:21:33 -0400

    fixed

--- axis-ptz/camera.py
+++ axis-ptz/camera.py
@@ -282,7 +282,8 @@ def on_message(client, userdata, message):
         bearingGood = setPan(update["bearing"])
         setTilt(update["elevation"])
         currentPlane = update
-    elif message.topic = 
+    elif message.topic == config_topic:
+        update_config(update)
     else:
         logging.info("Message: {} Object: {} Flight: {}".format(message.topic, object_topic, flight_topic))
 
