cdc02a2ff662d7d5f377807ed674bcba625333a1
jspeed-meyers <jmeyers@iqt.org>
2021-05-26 11:43:47 -0400

    Add customvox51 module
    Former-commit-id: be143175112e33abf767b0199f3cfb49632290e2

---
+++ ml-model/scripts/customvox51.py
@@ -0,0 +1,50 @@
+'''custom functionality related to voxel51 databases'''
+
+import glob
+import os
+
+import fiftyone as fo
+
+
+def build_image_list(file_path):
+    """Create a list of plane data dicts.
+    
+    Extract plane data from from jpg filenames.
+
+    The plane image filenames follow a strict naming convention.
+    For instance, ac760d_194_50_11882_2021-05-13-14-13-42.jpg translates to
+    ac760d - plane_id, aka ICAO 24
+    194 - plane bearing
+    50 - plane elevation
+    11882 - plane distance
+    TODO: external_id - ????
+    
+    Args:
+        file_path - Path to images
+        
+    Returns:
+        (list) image_list - a list of plane dict objects
+    """
+    image_list = []
+    for folder, _, files in os.walk(file_path):
+        for file in files:
+            if file.endswith(".jpg"):
+                # extract metadata for each plane from filename
+                image_filename = os.path.basename(file)
+                external_id = os.path.splitext(image_filename)[0]
+                image_path = os.path.abspath(os.path.join(folder, file))
+                plane_id = external_id.split("_")[0]
+                plane_bearing = external_id.split("_")[1]
+                plane_elevation = external_id.split("_")[2]
+                plane_distance = external_id.split("_")[3]
+                # place plane image data in a dict
+                item = {"file_path": image_path,
+                        "external_id": external_id,
+                        "bearing": plane_bearing,
+                        "elevation": plane_elevation,
+                        "distance": plane_distance,
+                        "icao24": plane_id}
+
+                image_list.append(item)
+
+    return image_list
