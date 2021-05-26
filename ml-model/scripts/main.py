9368b143866036d83b780332e72dd29743c4e506
jspeed-meyers <jmeyers@iqt.org>
2021-05-26 11:34:30 -0400

    Add read in config file functionality
    Former-commit-id: 29c0ce0051a24a17fc15fca57bc3f512acc4f60e

---
+++ ml-model/scripts/main.py
@@ -0,0 +1,27 @@
+'''Control logic for running skyscan data and model functionality.'''
+
+import configparser
+
+def read_config(config_file='config.ini'):
+    '''Read in config file values.
+
+    Use python standard library configparser module
+    to read in user-defined values. This enables configurability.
+
+    For more info on configparser, see here:
+    https://docs.python.org/3/library/configparser.html#module-configparser
+
+    Args:
+        config file (str) - name of config file
+
+    Returns:
+        config - a config object similar to a dict
+    '''
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    return config
+
+if __name__ == '__main__':
+    config = read_config()
+    print(config['filenames']['test'])
