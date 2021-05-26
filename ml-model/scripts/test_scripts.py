272b319175b3c5b425301f1d6c4e7f6188a864c8
jspeed-meyers <jmeyers@iqt.org>
2021-05-26 11:44:41 -0400

    Add test stub for build_image_list()

--- ml-model/scripts/test_scripts.py
+++ ml-model/scripts/test_scripts.py
@@ -1,7 +1,12 @@
 '''test all scripts.'''
 
+from customvox51 import build_image_list
 from main import read_config
 
+
+def test_build_image_list():
+    '''Test build_image_list()'''
+    pass
 
 def test_read_config():
     '''Test read_config().'''
