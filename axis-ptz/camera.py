f8ceb767db8f232d2193e2970c528b154ab7c2b7
Luke Berndt <lberndt@iqt.org>
2021-05-28 10:59:43 -0400

    Tracking is buttery smooth when the camera delay is 0

--- axis-ptz/camera.py
+++ axis-ptz/camera.py
@@ -340,7 +340,7 @@ def main():
     parser.add_argument('-p', '--axis-password', help="Password for the Axis camera", required=True)
     parser.add_argument('-a', '--axis-ip', help="IP address for the Axis camera", required=True)
     parser.add_argument('-s', '--camera-move-speed', type=int, help="The speed at which the Axis will move for Pan/Tilt (0-100)", default=50)
-    parser.add_argument('-d', '--camera-delay', type=float, help="How many seconds after issuing a Pan/Tilt command should a picture be taken", default=0.1)
+    parser.add_argument('-d', '--camera-delay', type=float, help="How many seconds after issuing a Pan/Tilt command should a picture be taken", default=0)
     parser.add_argument('-z', '--camera-zoom', type=int, help="The zoom setting for the camera (0-9999)", default=9999)
     parser.add_argument('-v', '--verbose',  action="store_true", help="Verbose output")
 
