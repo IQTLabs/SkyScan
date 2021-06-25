7ac729d6036c83965265ba05a0d6a334f251b977
John Speed Meyers <54914994+jspeed-meyers@users.noreply.github.com>
2021-06-25 08:53:22 -0700

    Add initial detection model script code(#34)
    * Add test for loading base models json
    
    * Perform black formatting on tests
    
    * Add load base models json function in detections.py
    
    * Add base_models.json to select pre-trained model
    
    * Add docstrings to load_base_models_json function
    
    * Add model-related config values
    
    * Add test for set_filenames function
    
    * Add pylint turn warnings off codes for tests
    
    * Add set_filenames function to detection.py
    
    * Add additional OS-level libraries for install to dockerfile
    
    Luke B. advised John Speed that these libraries are best installed
    via a dockerfile and not installed in the Python scripts themselves.
    I have therefore added these libraries to the dockerfile.
    
    * Add initial clone_tensorflow_repo code
    
    * Use simpled subprocess command
    
    * Update number of training steps default to 40000
    
    * Create consistent key naming for filepaths dict

--- ml-model/scripts/test_scripts.py
+++ ml-model/scripts/test_scripts.py
@@ -11,9 +11,12 @@ from customvox51 import (
     create_voxel51_dataset,
     normalize_single_model_value,
 )
-from labelbox_utils import merge_labelbox_dataset_with_voxel51
+from detection import load_base_models_json, set_filenames
+
+# from labelbox_utils import merge_labelbox_dataset_with_voxel51
 from main import read_config
 
+# pylint: disable=C0103, W0107
 
 # delete dataset first to create repeatable test environment
 try:
@@ -82,7 +85,36 @@ def test_normalize_model_values():
 
 def test_merge_labelbox_dataset_with_voxel51():
     """Test merge_labelbox_dataset_with_voxel51()"""
-    # TODO: determine if writing a simple test for this function is possible. Luke?
+    # Writing a test for this function is difficult. Do if time allows.
     # test_dataset = create_voxel51_dataset("test")
     # merge_labelbox_dataset_with_voxel51(test_dataset, "test/labelbox_export_test.json")
     pass
+
+
+def test_load_base_models_json():
+    """Test load_base_models_json()"""
+    test_models = load_base_models_json()
+    assert test_models["ssd_mobilenet_v2"]["batch_size"] == 24
+    assert (
+        test_models["efficientdet-d0"]["pretrained_checkpoint"]
+        == "efficientdet_d0_coco17_tpu-32.tar.gz"
+    )
+
+
+def test_set_filenames():
+    """Test set_filenames()"""
+    test_base_models = load_base_models_json()
+    TEST_TRAINING_NAME = "luke_burnt"
+    TEST_CHOSEN_MODEL = "ssd_mobilenet_v2"
+    test_filepaths = set_filenames(
+        test_base_models, TEST_TRAINING_NAME, TEST_CHOSEN_MODEL
+    )
+    assert test_filepaths["val_export_dir"] == "/tf/dataset-export/luke_burnt/val/"
+    assert (
+        test_filepaths["pretrained_checkpoint"]
+        == "ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz"
+    )
+    assert (
+        test_filepaths["pipeline_file"]
+        == "/tf/models/research/deploy/ssd_mobilenet_v2_320x320_coco17_tpu-8.config"
+    )
