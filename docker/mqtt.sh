24eeabd80b5ce32c5f8bdcdf1c95c7f637596b4a
Arizbeth Rojas <arojas@iqt.org>
2023-05-09 15:00:10 -0400

    authenticated users only on MQTT

--- docker/mqtt.sh
+++ docker/mqtt.sh
@@ -1,16 +1,10 @@
-#add-apt-repository ppa:certbot/certbot
-#apt install certbot mosquitto mosquitto-clients
+#!/bin/sh
 
 apk add certbot
 apk add certbot mosquitto mosquitto-clients 
 apk add nano
-#these Ubuntu but should be in Alpine 1 and 2 how to install certbot in alpine linux, get dockerfile to build with certbot 
 
-#ufw allow 80 
-#expose these ports in the docker file 
 certbot certonly --standalone --preferred-challenges http -d mqtt-skyscan
-#mosquitto_passwd -c /etc/mosquitto/passwd user
-# /b lets us pass the username and password in the command line, replacing -c
 mosquitto_passwd "$(cat /run/secrets/login_info_MQTT)"
 nano /etc/mosquitto/conf.d/default.conf 
 
