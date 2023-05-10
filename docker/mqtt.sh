8f22982cd0cb585345bf313d1b6fd9472e12773f
Arizbeth Rojas <arojas@iqt.org>
2023-05-10 16:04:06 -0400

    requires username and password for mqtt connection

--- docker/mqtt.sh
+++ docker/mqtt.sh
@@ -1,15 +1,27 @@
-#!/bin/sh
+#! /bin/sh
+unset HISTFILE
 
-apk add certbot
-apk add certbot mosquitto mosquitto-clients 
-apk add nano
+touch /etc/mosquitto/passwd
+touch /var/log/mosquitto.log
+chown mosquitto:mosquitto /var/log/mosquitto.log
+chown mosquitto:mosquitto /etc/mosquitto/passwd
+#mosquitto_passwd -b /etc/mosquitto/passwd $(cat /run/secrets/mqtt_user.txt) $(cat /run/secrets/mqtt_passwd.txt)
+mosquitto_passwd -b /etc/mosquitto/passwd $(cat /run/secrets/mqtt_user) $(cat /run/secrets/mqtt_passwd)
+/usr/sbin/mosquitto -v -c /etc/mosquitto/mosquitto.conf
 
-certbot certonly --standalone --preferred-challenges http -d mqtt-skyscan
-mosquitto_passwd "$(cat /run/secrets/login_info_MQTT)"
-nano /etc/mosquitto/conf.d/default.conf 
+# test with mosquitto_pub -h mqtt-server -u $(cat /run/secrets/mqtt_user) -P $(cat /run/secrets/mqtt_pass) -t "test" -m "Hats!"
 
-ufw allow 8883
-ufw allow 443
+#systemctl enable mosquitto.service
+#apk add certbot mosquitto mosquitto-clients 
+#apk add vim
 
-nano /etc/letsencrypt/renewal/mqtt-skyscan.conf
-certbot renew --dry-run
+#certbot certonly --standalone --preferred-challenges http -d mqtt-skyscan
+#mosquitto_passwd "$(cat /run/secrets/login_info_MQTT)"
+#cp mqtt.conf /etc/mosquitto/conf.d/default.conf 
+#vi /etc/mosquitto/conf.d/default.conf 
+
+#ufw allow 8883
+#ufw allow 443
+
+#vi /etc/letsencrypt/renewal/mqtt-skyscan.conf
+#certbot renew --dry-run
